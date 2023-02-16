81faa9a11308f237163eed648b2c39adf421fecb
ji11ian <123039281+ji11ian@users.noreply.github.com>
2023-02-16 09:55:30 +0800

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -2,32 +2,32 @@ public class Variables {
 
     public static void main(String[] args) {
 
-        // 1.
-        int Age = 33;
-        // 2.
-        System.out.println(33);
-        // 3.
-        double balance = 18.89;
-        // 4.
-        int dollars = double
-        // 5.
-        var dollarsRounded = 19;
-        // 6.
-        char letter = 'A'
-        // 7.
-        System.out.println('A');
-        // 8.
-        System.out.println(int = char 'A');
-        // 9.
-        int
-        // 10.
-        System.out.println('A');
-        // 11.
-        System.out.println(int 'A');
-        // 12.
-        /**
+    // 1.
+    int age = 33;
+    // 2.
+    System.out.println(age);
+    // 3.
+    double balance = 18.89;
+    // 4.
+    int dollars = (int) balance;
+    // 5.
+    int dollarsRounded = (int) Math.round(balance);
+    // 6.
+    char letter = 'A';
+    // 7.
+    System.out.println(letter);
+    // 8.
+    System.out.println((int) letter);
+    // 9.
+    letter++;
+    // 10.
+    System.out.println(letter);
+    // 11
+    System.out.println((int) letter);
+    // 12
+    /**
          * [question 6-11 is declare and initializing a letter to a value of A, then casting it to an integer, then printing out it  with increment ]
          */
-
-    }
+  }
 }
+
